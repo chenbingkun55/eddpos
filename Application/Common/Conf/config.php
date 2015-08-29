3205278e7fedec7be1f3b8036d644af8f82c59c2
chenbingkun55 <chenbingkun55@163.com>
2015-08-29 12:45:44 +0000

    + .htaccess * 重定向index.php 去除

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -5,6 +5,7 @@ return array(
     'LANG_AUTO_DETECT'      => false, // 自动侦测语言 开启多语言功能后有效
     'LANG_LIST'             => 'en-us', // 允许切换的语言列表 用逗号分隔
     'VAR_LANGUAGE'          => 'l', // 默认语言切换变量
+    'URL_MODEL'             => 2, //配合 Apache rewrite 消除URL里的index.php,需要开启 AllowOverride All
 
     //SESSION
     'SESSION_AUTO_START'    => true, //是否开启SESSION
